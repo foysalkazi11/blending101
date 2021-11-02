24c27b8962709ca7b9bcd568c80aa9e0bb8177ba
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2021-11-02 14:19:25 +0600

    formulate page start

---
+++ components/formulate/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewRecipe from './newRecipe/NewRecipe.component';
+
+const Formulate = () => {
+    return (
+        <div>
+            <NewRecipe />
+        </div>
+    )
+}
+
+export default Formulate
---
+++ pages/component/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Formulate from '../../components/formulate';
+
+const index = () => {
+    return (
+        <div>
+            <Formulate />
+        </div>
+    )
+}
+
+export default index
