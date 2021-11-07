2b111ad47d47910e79082be66b8a00456205c702
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2021-11-07 22:43:30 +0600

    formate code

--- theme/recipe/recipeItem/RecipeItem.component.tsx
+++ theme/recipe/recipeItem/RecipeItem.component.tsx
@@ -1,66 +1,69 @@
 /* eslint-disable @next/next/no-img-element */
-import React,{useState,useEffect} from 'react'
-import styles from './RecipeItem.module.scss'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import React, { useState, useEffect } from "react";
+import styles from "./RecipeItem.module.scss";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
-interface RecipeItemProps{
-    item:object
-	plusIcon?: boolean
-	dragIcon ? : boolean
-	deleteIcon ? : boolean
-	handleClick ?: (value:object)=>void
-	handleDelete ?: (id:number)=>void
+interface RecipeItemProps {
+  item: object;
+  plusIcon?: boolean;
+  dragIcon?: boolean;
+  deleteIcon?: boolean;
+  handleClick?: (value: object) => void;
+  handleDelete?: (id: number) => void;
 }
 
-const RecipeItem = (props:RecipeItemProps) => {
+const RecipeItem = (props: RecipeItemProps) => {
+  const {
+    item,
+    plusIcon = true,
+    dragIcon = true,
+    deleteIcon = false,
+    handleClick,
+    handleDelete,
+  } = props;
+  const [items, setItems] = useState({});
 
-	const {item,plusIcon = true,dragIcon = true,deleteIcon=false,handleClick,handleDelete} = props
-	const [items,setItems] = useState({})
+  useEffect(() => {
+    /* @ts-ignore */
+    if (item?.label) {
+      setItems(item);
+    }
+  }, []);
 
-	useEffect(() => {
-		{/* @ts-ignore */}
-		if (item?.label) {
-			setItems(item)
-		}
-		
-	}, [])
-	
-    return (
-        
-            <li className={styles.listContainer}>
-				{/* @ts-ignore */}
-            { items?.label }
+  return (
+    <li className={styles.listContainer}>
+      {/* @ts-ignore */}
+      {items?.label}
 
-											{dragIcon ?
-											<button className={styles.listContainer__draggableBtn}> 
-                                            <DragIndicatorIcon />
-											{/* <img src="icons/noun_drag_3124730.svg" alt="" />  */}
-											</button>
-											 :null}
+      {dragIcon ? (
+        <button className={styles.listContainer__draggableBtn}>
+          <DragIndicatorIcon />
+          {/* <img src="icons/noun_drag_3124730.svg" alt="" />  */}
+        </button>
+      ) : null}
 
-											{plusIcon ?
-											<button className={styles.listContainer__addBtn} onClick={()=>handleClick ? handleClick(items) : null} > 
-											<AddCircleOutlineIcon  />
-											</button>
-											 : null}
+      {plusIcon ? (
+        <button
+          className={styles.listContainer__addBtn}
+          onClick={() => (handleClick ? handleClick(items) : null)}
+        >
+          <AddCircleOutlineIcon />
+        </button>
+      ) : null}
 
-											{deleteIcon ?
-											<button 
-											className={styles.listContainer__addBtn} 
-											/* @ts-ignore */
-											onClick={() => handleDelete ? handleDelete(items?.id) : null} 
-											> 
-											<HighlightOffIcon  />
-											</button>
-											 : null}
-
-										</li>
-                                    
-   
-	
-    )
-}
+      {deleteIcon ? (
+        <button
+          className={styles.listContainer__addBtn}
+          /* @ts-ignore */
+          onClick={() => (handleDelete ? handleDelete(items?.id) : null)}
+        >
+          <HighlightOffIcon />
+        </button>
+      ) : null}
+    </li>
+  );
+};
 
 export default RecipeItem;
