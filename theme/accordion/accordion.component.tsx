f6f8cb85d68ff449f39d50f038a9171de82c6ba6
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2021-11-15 22:53:47 +0600

    make reusable accordion

---
+++ theme/accordion/accordion.component.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { FiPlusSquare, FiMinusSquare } from "react-icons/fi";
+import styles from "./accordion.module.scss";
+
+type CustomAccordionProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const CustomAccordion = ({ title, children }: CustomAccordionProps) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  return (
+    <Accordion
+      expanded={expanded === true}
+      onChange={() => setExpanded(!expanded)}
+      sx={{
+        boxShadow: "none",
+        "&.MuiAccordion-root:before": {
+          height: "0px !important",
+        },
+        "& .MuiAccordionSummary-root.Mui-expanded": {
+          minHeight: "0px !important",
+        },
+        "& .MuiAccordionSummary-content.Mui-expanded": {
+          margin: "10px 0px !important",
+        },
+        "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+          transform: "rotate(0deg)",
+        },
+        "& .MuiAccordionDetails-root": {
+          padding: "8px 16px 0px",
+        },
+      }}
+    >
+      <AccordionSummary
+        sx={{
+          flexDirection: "row-reverse",
+        }}
+        expandIcon={
+          expanded ? (
+            <FiMinusSquare className={styles.icon} />
+          ) : (
+            <FiPlusSquare className={styles.icon} />
+          )
+        }
+      >
+        <h5 className={styles.title}>{title}</h5>
+      </AccordionSummary>
+      <AccordionDetails>{children}</AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default CustomAccordion;
