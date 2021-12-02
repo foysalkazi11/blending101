181e26d7541dc4f7f5c7bff68836b4395daa8bf3
vampiree007 <vikrantgupta.psit@gmail.com>
2021-12-02 12:27:29 +0530

    all wiki trays added - partialy completed inggredient tray

--- components/sidetray/wiki/ingredient/ingredient.component.tsx
+++ components/sidetray/wiki/ingredient/ingredient.component.tsx
@@ -1,38 +1,108 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from 'react';
 import WikiTray from '../wikiTray.component';
 import Dropdown from 'react-dropdown';
 import styles from './ingredient.module.scss';
 
-export default function IngredientTrayComponent({title}) {
-    const options = [
-    'systems', 'two', 'three'
-    ];
-    const systems = [
-        'Circulatory',
-        'Cardiovascular',
-        'Digestive',
-        'Excretory',
-        'Endocrine',
-        'Integumentry',
-        'Exocrine',
-        'Immune',
-        'Lymphatic',
-        'Muscular'
-    ]
-    const defaultOption = options[0];
+export default function IngredientTrayComponent({ title }) {
+	const options = ['All', 'two', 'three'];
+	const [toggle, setToggle] = useState('one');
+	const defaultOption = options[0];
 
-    const onChange = () => {
+	const categories = ['Leafy Green', 'Collard'];
+	const defOption = categories[0];
 
-    }
-    
+	const onChange = () => {};
 
-    return (
-        <WikiTray title={title}>
-            <div className={styles.nutrition}>
-                <Dropdown options={options} onChange={onChange} value={defaultOption} placeholder="Select an option" controlClassName={styles.dropdown} />
-                <div>
-                </div>
-            </div>
-        </WikiTray>
-    )
+	const UiOne = () => (
+		<div className={styles.uiOne}>
+			<Dropdown
+				options={options}
+				onChange={onChange}
+				value={defaultOption}
+				placeholder="Select an option"
+				controlClassName={styles.dropdown}
+			/>
+			<div className={styles.nutrition__menu}>
+				<div className={styles.nutrition__menu__item}>
+					<div className={styles.nutrition__menu__item__image}>
+						<img src="/food/chard.png" alt="ingr" />
+					</div>
+					<p>Ingredients</p>
+				</div>
+				<div className={styles.nutrition__menu__item}>
+					<div className={styles.nutrition__menu__item__image}>
+                    <img src="/food/kale.png" alt="ingr" />
+					</div>
+					<p>Nutrition</p>
+				</div>
+				<div className={styles.nutrition__menu__item}>
+					<div className={styles.nutrition__menu__item__image}>
+                    <img src="/food/Dandelion.png" alt="ingr" />
+					</div>
+					<p>Health</p>
+				</div>
+                <div className={styles.nutrition__menu__item}>
+					<div className={styles.nutrition__menu__item__image}>
+                    <img src="/food/spinach.png" alt="ingr" />
+					</div>
+					<p>Ingredients</p>
+				</div>
+				<div className={styles.nutrition__menu__item}>
+					<div className={styles.nutrition__menu__item__image}>
+                    <img src="/food/collard_greens.png" alt="ingr" />
+					</div>
+					<p>Nutrition</p>
+				</div>
+				<div className={styles.nutrition__menu__item}>
+					<div className={styles.nutrition__menu__item__image}>
+                    <img src="/food/spinach.png" alt="ingr" />
+					</div>
+					<p>Health</p>
+				</div>
+			</div>
+		</div>
+	);
+
+	const UiTwo = () => (
+		<div className={styles.uiOne}>
+			<Dropdown
+				options={options}
+				onChange={onChange}
+				value={defOption}
+				placeholder="Select an option"
+				controlClassName={styles.dropdown}
+			/>
+		</div>
+	);
+
+	return (
+		<WikiTray title={title}>
+			<div className={styles.nutrition}>
+				<div className={styles.toggle}>
+					<div
+						className={
+							toggle === 'one'
+								? styles.toggle__child + ' ' + styles.active
+								: styles.toggle__child
+						}
+						onClick={() => setToggle('one')}
+					>
+						Pictures
+					</div>
+					<div
+						className={
+							toggle === 'two'
+								? styles.toggle__child + ' ' + styles.active
+								: styles.toggle__child
+						}
+						onClick={() => setToggle('two')}
+					>
+						Rankings
+					</div>
+				</div>
+				<div>{toggle === 'one' ? <UiOne /> : <UiTwo />}</div>
+			</div>
+		</WikiTray>
+	);
 }
