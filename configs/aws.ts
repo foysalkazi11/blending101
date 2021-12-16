27c7944edd289683397e1a6aac927de4ac035c7a
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2021-12-17 00:56:36 +0600

    start authencation with user valdiation

---
+++ configs/aws.ts
@@ -0,0 +1,6 @@
+export const awsconfig = {
+  aws_project_region: "us-east-2", // REGION
+  aws_cognito_region: "us-east-2", // REGION
+  aws_user_pools_id: "us-east-2_v2nak5mao", // ENTER YOUR USER POOL ID
+  aws_user_pools_web_client_id: "2h0rtfsn7rg3joc93a546b2h70", // ENTER YOUR CLIENT ID
+};
