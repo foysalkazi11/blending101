57bcdcc12ca5a764eb968e7a53c952f6998bd51c
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2022-02-11 12:36:38 +0600

    wip teg section

---
+++ components/sidetray/filter/checkboxOptions/CheckboxOptions.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useAppSelector } from "../../../../redux/hooks";
+import CustomCheckbox from "../../../../theme/checkbox/CustomCheckbox";
+import styles from "./CheckboxOptions.module.scss";
+
+const CheckboxOptions = () => {
+  const { dbUser } = useAppSelector((state) => state?.user);
+  const { recipeFilterByIngredientCategory } = useAppSelector(
+    (state) => state?.ingredients
+  );
+  const options = {
+    Collection: dbUser?.collections?.map((item) => item?.name),
+    Dynamic: ["Popular", "Recommended", "Latest"],
+  };
+  return (
+    <div className={styles.checkboxOptionsContainer}>
+      {options[recipeFilterByIngredientCategory]?.map((item, index) => {
+        return (
+          <div key={index} className={styles.singleCheckbox}>
+            <CustomCheckbox />
+            <p>{item}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CheckboxOptions;
