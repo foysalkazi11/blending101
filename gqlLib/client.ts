390b8cb42a8ad69808551dd1fa27fe8eaa51dead
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2022-02-21 14:39:40 +0600

    change gql client url default options

--- gqlLib/client.ts
+++ gqlLib/client.ts
@@ -3,11 +3,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://recipeblend.herokuapp.com/graphql",
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: "cache-and-network",
-    },
-  },
+  // defaultOptions: {
+  //   watchQuery: {
+  //     fetchPolicy: "cache-and-network",
+  //   },
+  // },
 });
 
 export default client;
