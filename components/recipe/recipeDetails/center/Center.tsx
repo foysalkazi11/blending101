2cd05b73839e23b372a79960789446bd20228a43
Aniket Yadav <gaurav.aniket1@gmail.com>
2022-04-01 17:27:34 +0530

    fix(slider-loader height)

--- components/recipe/recipeDetails/center/Center.tsx
+++ components/recipe/recipeDetails/center/Center.tsx
@@ -10,7 +10,10 @@ import { MdOutlineInfo, MdAdd, MdRemove } from "react-icons/md";
 import { BiBarChart } from "react-icons/bi";
 import { BsCartPlus } from "react-icons/bs";
 import { useAppDispatch } from "../../../../redux/hooks";
-import { setOpenCommentsTray, setToggleModal } from "../../../../redux/slices/sideTraySlice";
+import {
+  setOpenCommentsTray,
+  setToggleModal,
+} from "../../../../redux/slices/sideTraySlice";
 import Modal from "../../../../theme/modal/customModal/CustomModal";
 import ShareRecipeModal from "../shareRecipeModal/ShareRecipeModal";
 import SaveRecipe from "../saveRecipe/SaveRecipe";
@@ -62,7 +65,11 @@ const Center = ({
         <p className={styles.text}>
           {isReadMore ? text.slice(0, 300) : text},
           <span onClick={toggleReadMore} className={styles.read_or_hide}>
-            {isReadMore ? <span>&nbsp; {"Read More"}</span> : <span>&nbsp; {"Read Less"}</span>}
+            {isReadMore ? (
+              <span>&nbsp; {"Read More"}</span>
+            ) : (
+              <span>&nbsp; {"Read Less"}</span>
+            )}
           </span>
         </p>
       );
@@ -106,8 +113,14 @@ const Center = ({
         </div>
         <div className={styles.subMenu}>
           <div className={styles.alignItems}>
-            <div className={styles.recipeType}>{recipeDetails?.recipeBlendCategory?.name}</div>
-            <img src="/images/yummly-logo.png" alt="recipe_logo" className={styles.recipeLogo} />
+            <div className={styles.recipeType}>
+              {recipeDetails?.recipeBlendCategory?.name}
+            </div>
+            <img
+              src="/images/yummly-logo.png"
+              alt="recipe_logo"
+              className={styles.recipeLogo}
+            />
           </div>
           <div className={styles.alignItems}>
             <div className={styles.iconWithText}>
@@ -155,14 +168,21 @@ const Center = ({
                       }}
                     />
                     {img.image && (
-                      <Image src={img.image} alt="recipe_image" layout="fill" objectFit="contain" />
+                      <Image
+                        src={img.image}
+                        alt="recipe_image"
+                        layout="fill"
+                        objectFit="contain"
+                      />
                     )}
                   </div>
                 );
               })}
             </SlickSlider>
           ) : (
-            <div style={{ margin: "30px 0px" }}>
+            <div
+              className={styles.imageBox__loader}
+            >
               <CircularRotatingLoader />
             </div>
           )}
@@ -199,7 +219,9 @@ const Center = ({
             <div className={styles.count}>
               <button
                 onClick={() =>
-                  setCounter((pre) => (Number(pre) <= 1 ? Number(pre) : Number(pre) - 1))
+                  setCounter((pre) =>
+                    Number(pre) <= 1 ? Number(pre) : Number(pre) - 1
+                  )
                 }
               >
                 <MdRemove className={styles.icon} />
@@ -229,16 +251,23 @@ const Center = ({
           {recipeDetails?.ingredients ? (
             recipeDetails?.ingredients?.map((ingredient, index) => {
               return (
-                <div className={styles.singleIngredent} key={index + "ingredients_recipeDetails"}>
+                <div
+                  className={styles.singleIngredent}
+                  key={index + "ingredients_recipeDetails"}
+                >
                   <div className={styles.leftSide}>
                     <img src="/images/5-2-avocado-png-hd.png" alt="icon" />
                     <div>
                       {`${ingredient?.selectedPortion?.quantity * counter}
                       ${ingredient.selectedPortion.name} `}
                       {nutritionState &&
-                      ingredient?.ingredientId?._id === nutritionState[0].ingredientId?._id &&
+                      ingredient?.ingredientId?._id ===
+                        nutritionState[0].ingredientId?._id &&
                       singleElement === true ? (
-                        <span className={styles.leftSide__highlighted} style={{ color: "#fe5d1f" }}>
+                        <span
+                          className={styles.leftSide__highlighted}
+                          style={{ color: "#fe5d1f" }}
+                        >
                           {ingredient?.ingredientId?.ingredientName}
                         </span>
                       ) : (
@@ -249,7 +278,8 @@ const Center = ({
                     </div>
                   </div>
                   {nutritionState &&
-                  ingredient?.ingredientId?._id === nutritionState[0].ingredientId?._id &&
+                  ingredient?.ingredientId?._id ===
+                    nutritionState[0].ingredientId?._id &&
                   singleElement === true ? (
                     <div className={styles.iconGroup} style={{ display: "flex" }}>
                       <MdOutlineInfo className={styles.icon} />
@@ -298,7 +328,10 @@ const Center = ({
         {recipeDetails?.recipeInstructions ? (
           recipeDetails?.recipeInstructions?.map((step, index) => {
             return (
-              <div className={styles.steps} key={index + "recipeInstruction__recipeDetails"}>
+              <div
+                className={styles.steps}
+                key={index + "recipeInstruction__recipeDetails"}
+              >
                 <span>Step {index + 1}</span>
                 <p>{step}</p>
               </div>
