fb61c5585ecb5743a4d6094b0201ad902f54b1c1
WahidHoque <wahidhoquee@gmail.com>
2022-06-03 16:33:19 +0600

    Fixed the Redirect Logout

--- configs/aws.ts
+++ configs/aws.ts
@@ -7,7 +7,7 @@ const awsConfig = {
     domain: process.env.NEXT_PUBLIC_OAUTH_DOMAIN, // ENTER COGNITO DOMAIN LIKE: eru-test-pool.auth.eu-west-1.amazoncognito.com
     scope: ["phone", "email", "openid", "profile"],
     redirectSignIn: process.env.NEXT_PUBLIC_REDIRECT_SIGN, // ENTER YOUR SITE (enter http://localhost:8000 if testing frontend locally)
-    redirectSignOut: process.env.NEXT_PUBLIC_REDIRECT_SIGN, // ENTER YOUR SITE (enter http://localhost:8000 if testing frontend locally)
+    redirectSignOut: `${process.env.NEXT_PUBLIC_REDIRECT_SIGN}/login`, // ENTER YOUR SITE (enter http://localhost:8000 if testing frontend locally)
     responseType: "token",
   },
   federationTarget: "COGNITO_USER_POOLS",
