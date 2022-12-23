ed67663f5127a144e4316e9b9eda70983af15cf1
foysalkazi11 <foysalkazi11@gmail.com>
2022-12-23 16:02:52 +0600

    working blog collection

--- components/sidetray/blogCollectionTray/index.tsx
+++ components/sidetray/blogCollectionTray/index.tsx
@@ -1,13 +1,22 @@
+import { useLazyQuery, useMutation } from "@apollo/client";
 import { faRectangleHistory } from "@fortawesome/pro-regular-svg-icons";
 import { faBookmark } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ADD_NEW_BLOG_COLLECTION from "../../../gqlLib/blog/mutation/addNewBlogCllection";
+import DELETE_BLOG_COLLECTION from "../../../gqlLib/blog/mutation/deleteBlogCollection";
+import GET_ALL_BLOG_COLLECTIONS from "../../../gqlLib/blog/query/getAllBlogCollections";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import { setIsOpenBlogCollectionTray } from "../../../redux/slices/blogSlice";
+import {
+  setIsActiveBlogForCollection,
+  setIsOpenBlogCollectionTray,
+} from "../../../redux/slices/blogSlice";
 import CustomModal from "../../../theme/modal/customModal/CustomModal";
 import ToggleMenu from "../../../theme/toggleMenu/ToggleMenu";
+import notification from "../../utility/reactToastifyNotification";
 import AddCollectionModal from "../collection/addCollectionModal/AddCollectionModal";
 import IconForAddComment from "../common/iconForAddComment/IconForAddComment";
+import SingleCollection from "../common/singleCollection/SingleCollection";
 import TrayTag from "../TrayTag";
 import TrayWrapper from "../TrayWrapper";
 import styles from "./BlogCollectionTray.module.scss";
@@ -21,27 +30,162 @@ const BlogCollectionTray = ({
   showTagByDefaut,
 }: BlogCollectionTrayProps) => {
   const [openModal, setOpenModal] = useState(false);
-  const [input, setInput] = useState<any>({
+  const [input, setInput] = useState({
     image: null,
     name: "",
+    slug: "",
   });
+  const [menuIndex, setMenuIndex] = useState(0);
   const [isEditCollection, setIsEditCollection] = useState(false);
   const [collectionId, setCollectionId] = useState("");
-  const { isOpenBlogCollectionTray } = useAppSelector((state) => state?.blog);
+  const { isOpenBlogCollectionTray, isActiveBlogForCollection } =
+    useAppSelector((state) => state?.blog);
+  const memberId = useAppSelector((state) => state?.user?.dbUser?._id || "");
+  const [
+    getAllBlogCollections,
+    {
+      data: allCollectionData,
+      loading: allCollectionLoading,
+      error: allCollectionError,
+    },
+  ] = useLazyQuery(GET_ALL_BLOG_COLLECTIONS, {
+    fetchPolicy: "cache-and-network",
+  });
+  const [addNewBlogCollection, { loading: addNewBlogCollectionLoading }] =
+    useMutation(ADD_NEW_BLOG_COLLECTION);
+  const [deleteCollection, { loading: deleteCollectionLoading }] = useMutation(
+    DELETE_BLOG_COLLECTION,
+  );
   const dispatch = useAppDispatch();
 
+  // add or edit collection
+
+  const handleAddOrEditCollection = async () => {
+    try {
+      if (input?.name) {
+        if (isEditCollection) {
+          //  await editCollection({
+          //    variables: {
+          //      data: {
+          //        userEmail: dbUser?.email,
+          //        collectionId: collectionId,
+          //        newName: input?.name,
+          //      },
+          //    },
+          //  });
+          //  setOpenModal(false);
+          //  setInput({ image: null, name: "" });
+          setOpenModal(false);
+        } else {
+          await addNewBlogCollection({
+            variables: {
+              data: {
+                memberId,
+                name: input.name,
+                slug: input.slug,
+              },
+            },
+            update(cache, { data: { addNewBlogCollection } }) {
+              cache.writeQuery({
+                query: GET_ALL_BLOG_COLLECTIONS,
+                variables: { memberId },
+                data: {
+                  getAllBlogCollections: {
+                    blogCollections: [
+                      ...allCollectionData?.getAllBlogCollections
+                        ?.blogCollections,
+                      addNewBlogCollection,
+                    ],
+                  },
+                },
+              });
+            },
+          });
+        }
+
+        setOpenModal(false);
+        setInput({ image: null, name: "", slug: "" });
+        if (isEditCollection) {
+          notification("info", "Collection edit successfully");
+        } else {
+          notification("info", "Collection add successfully");
+        }
+      } else {
+        notification("info", "Please write collection name");
+      }
+    } catch (error) {
+      notification("error", "An error occurred");
+    }
+  };
   // open collection modal
   const addNewCollection = () => {
     setIsEditCollection(false);
-    setInput((pre) => ({ ...pre, name: "" }));
+    setInput((pre) => ({ ...pre, name: "", slug: "" }));
     setOpenModal(true);
   };
+
+  // handle delete collection
+  const handleDeleteCollection = async (collectionId: string) => {
+    try {
+      await deleteCollection({
+        variables: {
+          collectionId,
+          memberId,
+        },
+        update(cache, { data: { deleteBlogCollection } }) {
+          // const allCollectionData = cache.readQuery({
+          //   query: GET_ALL_BLOG_COLLECTIONS,
+          //   variables: { memberId },
+          // });
+
+          cache.writeQuery({
+            query: GET_ALL_BLOG_COLLECTIONS,
+            variables: { memberId },
+            data: {
+              getAllBlogCollections: {
+                blogCollections: [...deleteBlogCollection?.blogCollections],
+              },
+            },
+          });
+        },
+      });
+
+      //  dispatch(setCurrentCollectionInfo({ id: "", name: "All Recipes" }));
+      //  updateRecipe(activeRecipeId, {
+      //    collection: null,
+      //  });
+
+      notification("info", "Delete collection successfully");
+    } catch (error) {
+      notification("error", error?.message);
+    }
+  };
+
+  // edit a collection
+  const handleEditCollection = (id: string, name: string, slug: string) => {
+    setInput((pre) => ({
+      ...pre,
+      name,
+      slug,
+    }));
+    setIsEditCollection(true);
+    setCollectionId(id);
+    setOpenModal(true);
+  };
+
+  useEffect(() => {
+    if (isOpenBlogCollectionTray) {
+      getAllBlogCollections({ variables: { memberId } });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpenBlogCollectionTray]);
   return (
     <TrayWrapper
       showTagByDefaut={showTagByDefaut}
-      closeTray={() =>
-        dispatch(setIsOpenBlogCollectionTray(!isOpenBlogCollectionTray))
-      }
+      closeTray={() => {
+        dispatch(setIsActiveBlogForCollection(""));
+        dispatch(setIsOpenBlogCollectionTray(!isOpenBlogCollectionTray));
+      }}
       openTray={isOpenBlogCollectionTray}
       showPanle={showPanle}
       panleTag={(hover) => (
@@ -59,7 +203,6 @@ const BlogCollectionTray = ({
               icon={faRectangleHistory}
               className={styles.icon}
             />
-
             <p>Collections</p>
           </div>,
         ]}
@@ -69,13 +212,43 @@ const BlogCollectionTray = ({
         handleIconClick={addNewCollection}
         tooltipText="Add collection"
       />
-      <>BlogCollection</>
+      {allCollectionData?.getAllBlogCollections?.blogCollections?.map(
+        (collection, i) => {
+          const { _id, name, slug, collectionDataCount, image, blogs } =
+            collection;
+
+          return (
+            <SingleCollection
+              key={_id}
+              id={_id}
+              name={name}
+              slug={slug}
+              image={image || "/cards/food.png"}
+              collectionItemLength={collectionDataCount}
+              showMoreMenu={true}
+              handleDeleteCollection={handleDeleteCollection}
+              handleEditCollection={handleEditCollection}
+              index={i}
+              menuIndex={menuIndex}
+              setMenuIndex={setMenuIndex}
+              changeItemWithinCollection={
+                isActiveBlogForCollection ? true : false
+              }
+              isRecipeWithinCollection={blogs?.includes(
+                isActiveBlogForCollection,
+              )}
+              deleteCollectionLoading={deleteCollectionLoading}
+            />
+          );
+        },
+      )}
       <CustomModal open={openModal} setOpen={setOpenModal}>
         <AddCollectionModal
           input={input}
           setInput={setInput}
-          //   getCollectionsAndThemes={getCollectionsAndThemes}
           setOpenModal={setOpenModal}
+          handleToAddOrUpdateCollection={handleAddOrEditCollection}
+          isAddOrUpdateCollectionLoading={addNewBlogCollectionLoading}
         />
       </CustomModal>
     </TrayWrapper>
---
+++ components/sidetray/common/singleCollection/SingleCollection.tsx
@@ -0,0 +1,136 @@
+import {
+  faPencil,
+  faShareNodes,
+  faTrash,
+} from "@fortawesome/pro-regular-svg-icons";
+import { faEllipsisVertical } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React, { Dispatch, SetStateAction } from "react";
+import CustomCheckbox from "../../../../theme/checkbox/CustomCheckbox";
+import CircularRotatingLoader from "../../../../theme/loader/circularRotatingLoader.component";
+import useHover from "../../../utility/useHover";
+import styles from "./SingleMenu.module.scss";
+
+interface IndividualCollectionType {
+  name?: string;
+  image?: string;
+  slug?: string;
+  showMoreMenu?: boolean;
+  changeItemWithinCollection?: boolean;
+  isRecipeWithinCollection?: boolean;
+  id?: string;
+  handleClickCheckBox?: (e: React.SyntheticEvent, id: string) => void;
+  index?: number;
+  collectionItemLength?: number;
+  menuIndex?: number;
+  setMenuIndex?: Dispatch<SetStateAction<number>>;
+  handleDeleteCollection?: (id: string) => void;
+  handleEditCollection?: (id: string, name: string, slug: string) => void;
+  deleteCollectionLoading?: boolean;
+}
+
+const SingleCollection = ({
+  name = "All Recipes",
+  image = "/cards/food.png",
+  slug = "",
+  showMoreMenu = false,
+  changeItemWithinCollection = false,
+  isRecipeWithinCollection = false,
+  id = "",
+  handleClickCheckBox = () => {},
+  index,
+  collectionItemLength = 0,
+  menuIndex = 0,
+  setMenuIndex = () => {},
+  handleDeleteCollection = () => {},
+  handleEditCollection = () => {},
+  deleteCollectionLoading = false,
+}: IndividualCollectionType) => {
+  const [hoverRef, isHovered] = useHover();
+  const handleClick = (index: number) => {
+    if (menuIndex === index) {
+      setMenuIndex(index);
+    } else {
+      setMenuIndex(index);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.collection__child} key={id} ref={hoverRef}>
+        <Link href={`/collection/${slug}`} passHref>
+          <div className={styles.leftSide}>
+            <div className={styles.img}>
+              <div
+                className={styles.abs}
+                style={{
+                  backgroundImage: `url(${image})`,
+                }}
+              ></div>
+            </div>
+            <p>{name}</p>
+          </div>
+        </Link>
+        {showMoreMenu ? (
+          changeItemWithinCollection ? (
+            <div className={styles.checkBox}>
+              <CustomCheckbox
+                checked={isRecipeWithinCollection}
+                handleChange={(e) => handleClickCheckBox(e, id)}
+              />
+            </div>
+          ) : isHovered ? (
+            name === "My Favorite" ? (
+              <p style={{ marginRight: "10px" }}>{collectionItemLength}</p>
+            ) : (
+              <div
+                className={styles.rightSide}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleClick(index);
+                }}
+              >
+                {/* <MdMoreVert className={styles.moreIcon} /> */}
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+
+                <div
+                  className={`${styles.menu} ${
+                    menuIndex === index ? styles.showMenu : ""
+                  }`}
+                >
+                  <FontAwesomeIcon
+                    icon={faPencil}
+                    className={styles.icon}
+                    onClick={() => handleEditCollection(id, name, slug)}
+                  />
+                  {deleteCollectionLoading ? (
+                    <CircularRotatingLoader
+                      color="primary"
+                      style={{ fontSize: "16px" }}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      className={styles.icon}
+                      onClick={() => handleDeleteCollection(id)}
+                    />
+                  )}
+
+                  <FontAwesomeIcon
+                    icon={faShareNodes}
+                    className={styles.icon}
+                  />
+                </div>
+              </div>
+            )
+          ) : (
+            <p style={{ marginRight: "10px" }}>{collectionItemLength}</p>
+          )
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default SingleCollection;
