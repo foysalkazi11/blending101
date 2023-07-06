b526a1083a3f8fbd6099d1e21fe549ed975472e9
WahidHoque <wahidhoquee@gmail.com>
2023-07-07 01:55:06 +0600

    Pushed Extesnion

--- pages/extension.tsx
+++ pages/extension.tsx
@@ -10,24 +10,34 @@ const Extension = () => {
   );
 
   useEffect(() => {
-    const id = "ebbpnaajpojkhndmjmdjabgjmngjgmhm";
-    const token = session?.signInUserSession?.accessToken;
-    //@ts-ignore
-    chrome.runtime?.sendMessage(
-      id,
-      {
-        action: "AUTH_LOGIN",
-        payload: {
-          token: token?.jwtToken,
-          expiration_time: token?.exp,
-          email: session?.attributes?.email,
-          userId,
-          name: displayName,
-        },
-      },
-      () => {},
-    );
-  }, [displayName, session, userId]);
+    window.addEventListener("message", (e) => {
+      console.log(e);
+      // const data = JSON.parse(e.data);
+      // if (typeof data.name !== "undefined") {
+      //   localStorage.setItem("name", data);
+      //   console.log("Name is set: ", data.name);
+      // }
+    });
+  }, []);
+  // useEffect(() => {
+  //   const id = "ebbpnaajpojkhndmjmdjabgjmngjgmhm";
+  //   const token = session?.signInUserSession?.accessToken;
+  //   //@ts-ignore
+  //   chrome.runtime?.sendMessage(
+  //     id,
+  //     {
+  //       action: "AUTH_LOGIN",
+  //       payload: {
+  //         token: token?.jwtToken,
+  //         expiration_time: token?.exp,
+  //         email: session?.attributes?.email,
+  //         userId,
+  //         name: displayName,
+  //       },
+  //     },
+  //     () => {},
+  //   );
+  // }, [displayName, session, userId]);
 
   return <div>Extension</div>;
 };
