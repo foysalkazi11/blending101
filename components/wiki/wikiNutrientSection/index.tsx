42fc83b21018bf962e46b559f9465ade55494f7e
foysalkazi11 <foysalkazi11@gmail.com>
2023-07-26 10:23:49 +0600

    made side panel sticky

--- components/wiki/wikiNutrientSection/index.tsx
+++ components/wiki/wikiNutrientSection/index.tsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import ToggleMenu from "../../../theme/toggleMenu/ToggleMenu";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFerrisWheel, faList } from "@fortawesome/pro-light-svg-icons";
+import React from "react";
 import WikiNutritionPanel from "../wikiNutritionPanel/WikiNutritionPanel";
 import WikiThemeContainer from "../wikiTheme/wikiThemContainer";
 import generateDummyArray from "../../../helperFunc/array/generateDummyArray";
@@ -10,35 +7,17 @@ interface Props {
   checkActive: (id: string) => boolean;
   handleItemClick: (item: any, isExist: any) => void;
   scrollAreaMaxHeight?: number;
+  toggle?: number;
 }
 
 const WikiNutrientSection = ({
   checkActive,
   handleItemClick,
   scrollAreaMaxHeight,
+  toggle = 0,
 }: Props) => {
-  const [toggle, setToggle] = useState(0);
-
   return (
     <>
-      <ToggleMenu
-        setToggle={setToggle}
-        toggle={toggle}
-        toggleMenuList={[
-          <div key={"key0"} className="d-flex ai-center">
-            <FontAwesomeIcon icon={faList} style={{ marginRight: "5px" }} />
-            <p>List</p>
-          </div>,
-          <div key={"key1"} className="d-flex ai-center">
-            <FontAwesomeIcon
-              icon={faFerrisWheel}
-              style={{ marginRight: "5px" }}
-            />
-            <p>Themes</p>
-          </div>,
-        ]}
-        variant={"outlineSecondary"}
-      />
       {toggle === 0 && (
         <WikiNutritionPanel
           checkActiveNutrition={checkActive}
