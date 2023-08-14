34cc3888b229923432f008c5fbdf0958d4b6ede3
WahidHoquee <wahidhoquee@gmail.com>
2023-08-14 13:45:53 +0600

    refactored login feature

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,17 +2,8 @@ import React, { useEffect } from "react";
 import AuthScreen from "../theme/authScreen/authScreen.component";
 
 export default function Login(props) {
-  // // Syncing with Extension
-  // useEffect(() => {
-  //   window.addEventListener("message", (e) => {
-  //     // setData(JSON.stringify(e.data));
-  //     localStorage.setItem("extensionssss", e.data);
-  //   });
-  // }, []);
-
   return (
     <div>
-      {/* {} */}
       <AuthScreen type={"login"} />
     </div>
   );
