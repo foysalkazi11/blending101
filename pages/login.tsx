4ef10971fc8fae357d7906b0ab4e8a1f741d0bdc
WahidHoquee <wahidhoquee@gmail.com>
2023-08-16 02:02:50 +0600

    build check

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,6 +4,7 @@ import AuthScreen from "../theme/authScreen/authScreen.component";
 export default function Login(props) {
   return (
     <div>
+      HELLO LOGIN
       <AuthScreen type={"login"} />
     </div>
   );
