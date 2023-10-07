869e4d46132122b179cda4f202cf89d052c21394
WahidHoquee <wahidhoquee@gmail.com>
2023-10-07 14:30:49 +0600

    Added Top Ingredients

--- modules/app/graphql/Ingredients.ts
+++ modules/app/graphql/Ingredients.ts
@@ -1,10 +1,16 @@
 import { gql } from "@apollo/client";
 
+export const TOP_INGREDIENT_FIELDS = gql`
+  fragment TopIngredientFields on TopIngredientData {
+    icon: featuredImage
+    label: name
+    quantity: count
+  }
+`;
+
 export const GET_INGREDIENTS = gql`
   query GetIngredients($classType: String!) {
-    filterIngredientByCategoryAndClass(
-      data: { ingredientCategory: $classType, IngredientClass: 0 }
-    ) {
+    filterIngredientByCategoryAndClass(data: { ingredientCategory: $classType, IngredientClass: 0 }) {
       _id
       ingredientName
       featuredImage
@@ -19,9 +25,7 @@ export const GET_INGREDIENTS = gql`
 
 export const GET_INGREDIENTS_RXFACT = gql`
   query GetIngredientsFact($ingredients: [BlendIngredientInfo!]!) {
-    getIngredientsFact: getNutrientsListAndGiGlByIngredientsForScrappingPanel(
-      ingredientsInfo: $ingredients
-    ) {
+    getIngredientsFact: getNutrientsListAndGiGlByIngredientsForScrappingPanel(ingredientsInfo: $ingredients) {
       nutrients {
         name
         units
