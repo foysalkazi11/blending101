2f4652c9ca169eb3ad428e51c1f009e393d78f96
foysalkazi11 <foysalkazi11@gmail.com>
2023-10-30 15:27:56 +0600

    wiki filter bug fixed

--- gqlLib/blog/query/getAllGeneralBlogForClient.ts
+++ gqlLib/blog/query/getAllGeneralBlogForClient.ts
@@ -1,8 +1,20 @@
 import { gql } from "@apollo/client";
 
 const GET_ALL_GENERAL_BLOG_FOR_CLIENT = gql`
-  query GetAllGeneralBlogForClient($memberId: String!, $currentDate: String!, $page: Float, $limit: Float) {
-    getAllGeneralBlogForClient(memberId: $memberId, currentDate: $currentDate, page: $page, limit: $limit) {
+  query GetAllGeneralBlogForClient(
+    $memberId: String!
+    $currentDate: String!
+    $categories: [String!]
+    $page: Float
+    $limit: Float
+  ) {
+    getAllGeneralBlogForClient(
+      memberId: $memberId
+      currentDate: $currentDate
+      categories: $categories
+      page: $page
+      limit: $limit
+    ) {
       blogs {
         _id
         coverImage
