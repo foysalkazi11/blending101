d43ffa7a614341d0b62c655fd2fd57323763f45f
foysalkazi11 <foysalkazi11@gmail.com>
2023-11-06 10:42:34 +0600

    change Head meta tag at recipe deatils

--- pages/recipe/recipe_details/[[...params]]/index.tsx
+++ pages/recipe/recipe_details/[[...params]]/index.tsx
@@ -18,6 +18,7 @@ import client from "gqlLib/client";
 import GET_A_RECIPE from "gqlLib/recipes/queries/getRecipeDetails";
 import mapIngredientStatus from "helperFunc/mapIngredientStatus";
 import { setDetailsARecipe } from "redux/slices/recipeSlice";
+import Head from "next/head";
 
 export const getServerSideProps = (async (context) => {
   const { params = [], token = "" } = context.query;
@@ -113,12 +114,51 @@ const Index = ({ recipe }: InferGetServerSidePropsType<typeof getServerSideProps
 
   return (
     <>
-      <Meta
+      {/* <Meta
         title={recipe?.recipeId?.name}
         description={recipe?.recipeId?.description}
         ogImage={recipe?.recipeId?.originalVersion?.selectedImage || recipe?.recipeId?.image[0]?.image}
         url={window?.location?.href || ""}
-      />
+      /> */}
+      <Head>
+        <title>{recipe?.recipeId?.name}</title>
+        <meta name="description" content={recipe?.recipeId?.description} />
+
+        {/* Open Graph tags */}
+        <meta
+          property="og:url"
+          content={`https://app.blending101.com/recipe/recipe_details/${recipe?.recipeId?._id}`}
+          key="ogurl"
+        />
+        <meta property="og:type" content="website" key="ogtype" />
+        <meta property="og:site_name" content={"blending101"} key="ogsitename" />
+        <meta property="og:title" content={recipe?.recipeId?.name} key="ogtitle" />
+        <meta property="og:description" content={recipe?.recipeId?.description} key="ogdesc" />
+        <meta
+          property="og:image"
+          content={recipe?.recipeId?.originalVersion?.selectedImage || recipe?.recipeId?.image[0]?.image}
+          key="ogimage"
+        />
+        {/* <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" /> */}
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="app.blending101.com" />
+        <meta
+          property="twitter:url"
+          content={`https://app.blending101.com/recipe/recipe_details/${recipe?.recipeId?._id}`}
+        />
+        <meta property="twitter:domain" content="garimadisposal.in" />
+        <meta name="twitter:title" content={recipe?.recipeId?.name} />
+        <meta name="twitter:description" content={recipe?.recipeId?.description} />
+        <meta
+          name="twitter:image"
+          content={recipe?.recipeId?.originalVersion?.selectedImage || recipe?.recipeId?.image[0]?.image}
+        />
+
+        {/* {twitterHandle && <meta name="twitter:creator" content={twitterHandle} />} */}
+      </Head>
       <RecipeDetails
         recipeData={detailsARecipe}
         nutritionData={recipeBasedNutrition ? JSON.parse(recipeBasedNutrition) : []}
